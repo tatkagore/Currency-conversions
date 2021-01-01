82a7d594ccad4611bf0ee89742069eec2437a19d
Tatiana Simmer <tatkagore@gmail.com>
2021-01-01 01:27:07 +0100

    Initial Commit

---
+++ Shared/Currency_conversionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Currency_conversionsApp.swift
+//  Shared
+//
+//  Created by Tatiana Simmer on 01/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct Currency_conversionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
